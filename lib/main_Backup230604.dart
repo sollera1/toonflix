69f58545bbdac0c6fd64708422083b2db2d121ad
Sollera <pjh27062706@naver.com>
2023-06-06 19:56:05 +0900

    웹툰 목록 화면 및 클릭 시 새창 생성

--- lib/main_Backup230604.dart
+++ lib/main_Backup230604.dart
@@ -1,4 +1,4 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
@@ -21,26 +21,6 @@ class App extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-//2023.06.04
-*/
-
-import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
-
-void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
       home: HomeScreen(),
     );
   }
